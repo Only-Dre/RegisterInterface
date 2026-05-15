1a07061c4127746e8617ee06c3c4ad8f95524560
Only-Dre <workofdre@proton.me>
2026-05-15 20:42:01 -0300

    Primeiro commit.

---
+++ Interface-Reg/Form1.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace Interface_Reg
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.txtDescricao = new System.Windows.Forms.TextBox();
+            this.txtQuantidade = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnCadastrar = new System.Windows.Forms.Button();
+            this.btnListar = new System.Windows.Forms.Button();
+            this.btnExcluir = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.dgvItens = new System.Windows.Forms.DataGridView();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItens)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.DarkGray;
+            this.panel1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Location = new System.Drawing.Point(-2, -3);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(804, 84);
+            this.panel1.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 35.25F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(206, 15);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(409, 54);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Cadastro de Itens";
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(38, 150);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(232, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // txtDescricao
+            // 
+            this.txtDescricao.Location = new System.Drawing.Point(293, 150);
+            this.txtDescricao.Name = "txtDescricao";
+            this.txtDescricao.Size = new System.Drawing.Size(232, 20);
+            this.txtDescricao.TabIndex = 2;
+            // 
+            // txtQuantidade
+            // 
+            this.txtQuantidade.Location = new System.Drawing.Point(545, 150);
+            this.txtQuantidade.Name = "txtQuantidade";
+            this.txtQuantidade.Size = new System.Drawing.Size(116, 20);
+            this.txtQuantidade.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F);
+            this.label1.Location = new System.Drawing.Point(34, 127);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Nome";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F);
+            this.label3.Location = new System.Drawing.Point(289, 127);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(80, 20);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "Descrição";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.5F);
+            this.label4.Location = new System.Drawing.Point(541, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(92, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Quantidade";
+            // 
+            // btnCadastrar
+            // 
+            this.btnCadastrar.Location = new System.Drawing.Point(38, 251);
+            this.btnCadastrar.Name = "btnCadastrar";
+            this.btnCadastrar.Size = new System.Drawing.Size(113, 31);
+            this.btnCadastrar.TabIndex = 7;
+            this.btnCadastrar.Text = "Cadastrar";
+            this.btnCadastrar.UseVisualStyleBackColor = true;
+            this.btnCadastrar.Click += new System.EventHandler(this.btnCadastrar_Click);
+            // 
+            // btnListar
+            // 
+            this.btnListar.Location = new System.Drawing.Point(199, 251);
+            this.btnListar.Name = "btnListar";
+            this.btnListar.Size = new System.Drawing.Size(113, 31);
+            this.btnListar.TabIndex = 8;
+            this.btnListar.Text = "Listar";
+            this.btnListar.UseVisualStyleBackColor = true;
+            this.btnListar.Click += new System.EventHandler(this.btnListar_Click);
+            // 
+            // btnExcluir
+            // 
+            this.btnExcluir.Location = new System.Drawing.Point(647, 251);
+            this.btnExcluir.Name = "btnExcluir";
+            this.btnExcluir.Size = new System.Drawing.Size(113, 31);
+            this.btnExcluir.TabIndex = 9;
+            this.btnExcluir.Text = "Excluir";
+            this.btnExcluir.UseVisualStyleBackColor = true;
+            this.btnExcluir.Click += new System.EventHandler(this.btnExcluir_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(38, 176);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(113, 31);
+            this.btnLimpar.TabIndex = 10;
+            this.btnLimpar.Text = "Limpar campos";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // dgvItens
+            // 
+            this.dgvItens.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.Sunken;
+            this.dgvItens.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvItens.Location = new System.Drawing.Point(38, 288);
+            this.dgvItens.Name = "dgvItens";
+            this.dgvItens.Size = new System.Drawing.Size(722, 150);
+            this.dgvItens.TabIndex = 11;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvItens);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnExcluir);
+            this.Controls.Add(this.btnListar);
+            this.Controls.Add(this.btnCadastrar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtQuantidade);
+            this.Controls.Add(this.txtDescricao);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvItens)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.TextBox txtDescricao;
+        private System.Windows.Forms.TextBox txtQuantidade;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnCadastrar;
+        private System.Windows.Forms.Button btnListar;
+        private System.Windows.Forms.Button btnExcluir;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.DataGridView dgvItens;
+    }
+}
+
---
+++ Interface-Reg/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+// Library de MySQL
+using MySql.Data.MySqlClient;
+
+namespace Interface_Reg
+{
+    public partial class Form1 : Form
+    {
+
+        // String de conexão com o banco de dados - atualmente rodando em Docker
+        string connection = "server=localhost;port=3307;database=cadastro_db;uid=usuario;pwd=senha123;";
+
+        // Método de busca de itens no DB e exibição no Grid
+        private void ListItems()
+        {
+            using (MySqlConnection conn = new MySqlConnection(connection))
+            {
+                try
+                {
+                    conn.Open(); // .Open -> Abre o banco de dados com a conexão settada
+
+                    // Comando SQL para seleção de todos os itens
+                    string sql = "SELECT id, nome, descricao, quantidade FROM itens";
+
+                    // Mapeamento e ponte para obentção de dados SQL
+                    MySqlDataAdapter adapter = new MySqlDataAdapter(sql, conn);
+
+                    // Armazenamento de dados na memória
+                    DataTable tabela = new DataTable();
+
+                    // Preenchimento da tabela com os dados recebidos do banco
+                    adapter.Fill(tabela);
+
+                    // .DataSouce -> Define de onde vêm os dados recebidos
+                    dgvItens.DataSource = tabela;
+                }
+                catch(Exception ex) 
+                {
+                    MessageBox.Show("Erro ao listar itens: " + ex.Message);
+                }
+            }
+
+        }
+
+        // Limpeza de campos da tela
+        private void LimparCampos()
+        {
+            txtNome.Clear();
+            txtDescricao.Clear();
+            txtQuantidade.Clear();
+
+            // .Focus define a entrada a ser priorizada
+            txtNome.Focus();
+        }
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCadastrar_Click(object sender, EventArgs e)
+        {
+            // Verificação de campos obrigatórios preenchidos
+            if (txtNome.Text == "" || txtQuantidade.Text == "")
+            {
+                MessageBox.Show("Preencha os dados necessários: nome, quantidade");
+                return;
+            }
+
+            // Verificação de inserção de número inteiro na quantidade digitada
+            if (!int.TryParse(txtQuantidade.Text, out int quantidade))
+            {
+                MessageBox.Show("Digite uma quantidade válida.");
+                return;
+            }
+
+            // Criação de Conexão com o banco
+            using (MySqlConnection conn = new MySqlConnection(connection))
+            {
+                try
+                {
+                    // Abre conexão
+                    conn.Open();
+
+                    // Comando SQL para inserção de dados
+                    string sql = "INSERT INTO itens (nome, descricao, quantidade) VALUES (@nome, @descricao, @quantidade)";
+
+                    // Cria o comando SQL - MySqlCommand executa os Sql
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                    // Substitui os parâmetros pelos valores inseridos da interface
+                    // Pega os parâmetros vindos de um SQL, e adiciona valores: @nome <- -> txtNome.Text
+                    cmd.Parameters.AddWithValue("@nome", txtNome.Text);
+                    cmd.Parameters.AddWithValue("@descricao", txtDescricao.Text);
+                    cmd.Parameters.AddWithValue("@quantidade", txtQuantidade.Text);
+
+                    // Executa a inserção na conexão e retorna a quantia de linhas afetadas
+                    cmd.ExecuteNonQuery();
+
+                    MessageBox.Show(
+                        "O item foi cadastrado com sucesso! " + txtNome.Text,
+                        "Confirmação",
+                        MessageBoxButtons.OK,
+                        MessageBoxIcon.None
+                    );
+
+
+                    // Limpa os campos
+                    LimparCampos();
+
+                    // Atualiza a tabela na interface
+                    ListItems();
+                }
+                catch (Exception ex) 
+                {
+                    MessageBox.Show("Erro ao cadastrar item: " + ex.Message);
+                }
+            }
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            // Chama o método de listagem
+            ListItems();
+        }
+
+        private void btnExcluir_Click(object sender, EventArgs e)
+        {
+            // Verifica a seleção de uma linha
+            if (dgvItens.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Selecione um item para excluir.");
+                return;
+            }
+
+            // Pega o ID do item selecionado
+            // .SelectedRows[0] -> Reconhece a linha 0 como selecionada
+            // .Cells["id"] -> Identifica que, na linha celecionada, é obtido/definido o valor da célula id 
+            int id = Convert.ToInt32(dgvItens.SelectedRows[0].Cells["id"].Value);
+
+            DialogResult resposta = MessageBox.Show(
+                "Deseja realmente excluir este item?",
+                "Confirmação",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question
+            );
+
+            // Caso selecione a exclusão:
+            if (resposta == DialogResult.Yes)
+            {
+                using (MySqlConnection conn = new MySqlConnection(connection))
+                {
+                    try
+                    {
+                        conn.Open();
+
+                        // Execução de deletar via ID
+                        string sql = "DELETE FROM itens WHERE id = @id";
+
+                        MySqlCommand cmd = new MySqlCommand(sql, conn);
+                        cmd.Parameters.AddWithValue("@id", id);
+
+                        // Método que faz com que não haja retorno de qualquer valor
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Item excluído com sucesso.");
+
+                        ListItems();
+                    }
+                    catch (Exception ex)
+                    {
+                        MessageBox.Show("Erro ao excluir item: " + ex.Message);
+                    }
+                }
+            }
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            LimparCampos();
+        }
+    }
+}
